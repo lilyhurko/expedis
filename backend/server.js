59c899d8b61de9933c265899c74e03d7360b40a7
Liliia Hurko <130755897+lilyhurko@users.noreply.github.com>
2025-12-05 19:35:49 +0100

    fix server.js after merge

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,8 @@
-require('dotenv').config(); 
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const cors = require('cors'); 
-const bodyParser = require('body-parser'); 
- 
-const offerRoutes = require('./routes/offers');   
-const airportsRouter = require("./routes/airports"); 
-const commentRoutes = require('./routes/comments'); 
-const authRoutes = require('./routes/auth');  
-const userRoutes = require('./routes/users'); 
-const protectedRoutes = require('./routes/protected'); 
-const flightConnectionsRoutes = require("./routes/flightConnection"); 
-const hotelRoutes = require('./routes/hotels'); 
-const walletRoutes = require('./routes/wallet'); 
-const bookingRoutes = require('./routes/bookings'); 
-const adminRoutes = require('./routes/admin'); 
-const carrentRoutes = require('./routes/carrent'); 
- 
-const app = express(); 
-const port = process.env.PORT || 5001; 
- 
-app.use(cors()); 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
- 
-app.use('/images', express.static('public/images')); 
- 
-console.log("Mounting routes..."); 
-app.use("/api/airports", airportsRouter); 
-app.use('/api/offers', offerRoutes);   
-app.use('/api/comments', commentRoutes); 
-app.use('/api/auth', authRoutes); 
-app.use('/api/users', userRoutes); 
-app.use('/api/protected', protectedRoutes); 
-app.use("/api/flight-connections", flightConnectionsRoutes); 
-app.use('/api/hotels', hotelRoutes); 
-app.use('/api/admin', adminRoutes); 
-app.use('/api/wallet', walletRoutes); 
-app.use('/api/bookings', bookingRoutes); 
-app.use('/api/cars', carrentRoutes); 
-app.use('/api/chat', require('./routes/chat')); 
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const offerRoutes = require('./routes/offers');  
 const airportsRouter = require("./routes/airports");
