fbdf88901ffd0afc37ffb3119bf88f136d80268b
Liliia Hurko <130755897+lilyhurko@users.noreply.github.com>
2025-12-05 19:26:13 +0100

    Delete comments

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,12 +1,9 @@
-// src/setupTests.js
 import '@testing-library/jest-dom';
 
-// 1. Поліфіли для TextEncoder (React 18+)
 const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-// 2. Мок для fetch (ОБОВ'ЯЗКОВО)
 global.fetch = jest.fn(() =>
   Promise.resolve({
     ok: true,
@@ -14,7 +11,6 @@ global.fetch = jest.fn(() =>
   })
 );
 
-// 3. Мок для matchMedia (для слайдерів)
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: jest.fn().mockImplementation(query => ({
